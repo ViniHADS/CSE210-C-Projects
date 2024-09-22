c415e72418a7066087b0fe240c1b4147ebb218c7
vinicius <elderaraujo228@gmail.com>
2024-09-21 21:59:02 -0300

    I've added a sample for the activity from the first week, VSC - Articulate.

---
+++ VSC_Sample.cs
@@ -0,0 +1,7 @@
+class Program
+{
+    static void main(string[] args)
+    {
+        Console.WriteLine("Hello, My name is Vinicius Henrique Araujo Da Silva, but you all can call me Vini.")
+    }
+}
