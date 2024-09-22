7a423ae1f0140f199e4d227e8ed5fd34861f7176
vinicius <elderaraujo228@gmail.com>
2024-09-22 02:04:19 -0300

    Prep3 is completed.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,33 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //  Console.Write("Enter the magic number: ");
+        //  int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 10);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess a number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
     }
 }
