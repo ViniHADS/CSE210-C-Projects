2fa28e5f454d5060f78f2679a52c524a6ecb6abd
vinicius <elderaraujo228@gmail.com>
2024-09-21 22:15:35 -0300

    Changed the phrase.

--- VSC_Sample.cs
+++ VSC_Sample.cs
@@ -2,6 +2,6 @@ class Program
 {
     static void main(string[] args)
     {
-        Console.WriteLine("Hello, My name is Vinicius Henrique Araujo Da Silva, but you all can call me Vini.")
+        Console.WriteLine("Hi I'm Vini.")
     }
 }
