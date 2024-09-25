a3bc91ee27f55abd81bfca4d3c4a83a3ccc23e58
vinicius <elderaraujo228@gmail.com>
2024-09-25 15:54:38 -0300

    updated the program.cs and included Job.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,12 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job j1 = new Job();
+        j1._jobTitle = "ALIMENTADOR DE LINHA DE PRODUÇÃO";
+        j1._company = "YAPP BRASIL LTDA";
+        j1._startYear = 2019;
+        j1._endYear = 2021;
     }
 }
