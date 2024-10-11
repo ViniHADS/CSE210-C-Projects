cc80f9b64b6a2d16c2ff4e5f4fb5b112e1c2efe3
vinicius <elderaraujo228@gmail.com>
2024-10-11 00:45:38 -0300

    Updated the foundation 1, program.cs was finished.

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Creating videos
+        Video video1 = new Video("How to Program in C#", "John Smith", 360);
+        video1.AddComment(new Comment("Alice", "Great video!"));
+        video1.AddComment(new Comment("Carlos", "Very informative, thanks!"));
+        video1.AddComment(new Comment("Maria", "Loved the content."));
+
+        Video video2 = new Video("Learning Python", "Maria Johnson", 420);
+        video2.AddComment(new Comment("Peter", "Excellent!"));
+        video2.AddComment(new Comment("Anna", "Super useful!"));
+        video2.AddComment(new Comment("Lucas", "I loved it!"));
+
+        Video video3 = new Video("JavaScript Tips", "Paula Davis", 300);
+        video3.AddComment(new Comment("Robert", "I liked the tips!"));
+        video3.AddComment(new Comment("Julia", "Very good!"));
+
+        // Adding videos to a list
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        // Iterating through the list of videos
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Duration: {video.Duration} seconds");
+            Console.WriteLine($"Number of comments: {video.GetCommentCount()}");
+
+            // Listing comments
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.Name}: {comment.Text}");
+            }
+
+            Console.WriteLine(); // Blank line for separation
+        }
     }
 }
---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class Comment
+{
+    public string Name { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+}
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Duration { get; set; } // in seconds
+    private List<Comment> comments;
+
+    public Video(string title, string author, int duration)
+    {
+        Title = title;
+        Author = author;
+        Duration = duration;
+        comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+}
